095f94f452c8004ae1c3e88a63a335e826c49b6e
Pradeshrai227 <116314058+Pradeshrai227@users.noreply.github.com>
2023-01-19 23:02:15 +0000

    Better

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import random
 
-options = ["Rock", "Paper", "Scissors"]
+options = ["rock", "paper", "scissors"]
 computer = (random.choice(options))
 user = None
+0<1
+1<2
+0>2
+
 
 while user not in options:
-  user = input("Pick Rock, Paper, or Scissors: ")
+  user = input("Pick Rock, Paper, or Scissors: ").lower()
 print("You Picked: " + user)
 print("I Picked: " + computer)
 if user == computer:
   print("Tie Lame")
-elif user != computer:
-  print("You are Loser")
-  
+
+
+elif user == 0 and computer == 2:
+  print("WOW it Worked You're a Winner")
+
+elif user == 1 and computer ==2:
+  print("Goodjob you LOSE!")
